6f40ad19138e4aa9339550f4d937f8c31999d8c2
Riko Riswandha F P <rikorisw@gmail.com>
2019-05-04 20:06:19 +0800

    Add parcel to projects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-/* global React ReactDOM */
-
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [

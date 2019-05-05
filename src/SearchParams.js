6b437cdc22701afae800010c654a93e79e9b15ff
Riko Riswandha F P <rikorisw@gmail.com>
2019-05-05 14:42:30 +0800

    Add hook effects

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
-import { ANIMALS } from "petfinder-client";
+import React, { useState, useEffect } from "react";
+import pf, { ANIMALS } from "petfinder-client";
 import useDropdown from "./useDropdown";
+
+const petfinder = pf();
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
-  const [breeds, setBreeds] = useState(["Havanese", "Maltese", "Bichon Frise"]);
+  const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "Havanese", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    setBreed("");
+    setBreeds([]);
+    petfinder.breed.list({ animal }).then(res => {
+      setBreeds(res.petfinder.breeds.breed);
+    }, console.error);
+  }, [animal]);
 
   return (
     <div className="search-params">
